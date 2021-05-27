301dc508ec34dab5f444e710b7bfcacdc95c897b
Kp <kp_1@outlook.com>
2021-05-27 19:58:04 +0400

    service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,3 @@
-const CACHE_NAME = 'static-cache-v2';
-const DATA_CACHE_NAME = 'data-cache-v1';
-
 // TODO: add listener and handler to cache static assets
 const FILES_TO_CACHE = [
   '/',
@@ -12,45 +9,64 @@ const FILES_TO_CACHE = [
   '/assets/icons/icon-512x512.jpg',
 ];
 
-// TODO: add listener and handler to retrieve static assets from the Cache Storage in the browser
-// self.addEventListener('install', (event) => {
-//   event.waitUntil(
-//     caches.open('static').then((cache) => {
-//       return cache.addAll(filesToCache);
-//     })
-//   );
-//   self.skipWaiting();
-// });
-// self.addEventListener('fetch', (event) => {
-//   event.respondWith(
-//     caches.match(event.request).then((response) => {
-//       return response || fetch(event.reqest);
-//     })
-//   );
-// });
+const CACHE_NAME = 'static-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v1';
 
 // install event handler
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('static').then((cache) => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './assets/css/style.css',
-        './assets/images/brandenburg.jpg',
-        './assets/images/reichstag.jpg',
-        './assets/images/map.jpg',
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE).then(self.skipWaiting());
     })
   );
-  console.log('Install');
-  self.skipWaiting();
 });
-// retrieve assets from cache
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', (event) => {
+  const currentCaches = [CACHE_NAME, DATA_CACHE_NAME];
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter(
+          (cacheName) => !currentCaches.includes(cacheName)
+        );
+      })
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
+
+// fetch assets from cache
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(DATA_CACHE_NAME).then((cache) => {
+          return fetch(event.request).then((response) => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
+  // if the request is not for the API, serve static assets using "offline-first" approach.
+  // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
+  evt.respondWith(
+    caches.match(evt.request).then(function (response) {
+      return response || fetch(evt.request);
     })
   );
 });
