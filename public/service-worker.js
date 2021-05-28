cac96554c49e39829578a821b5b3b4331eb2be88
Kp <kp_1@outlook.com>
2021-05-28 13:01:00 +0400

    Service-worker.js file updates

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,26 +1,52 @@
-var CACHE_NAME = 'my-site-cache-v1';
-const DATA_CACHE_NAME = 'data-cache-v1';
-var urlsToCache = [
+const FILES_TO_CACHE = [
   '/',
+  '/manifest.webmanifest',
   '/indexedDB.js',
   '/index.js',
-  '/manifest.json',
   '/styles.css',
   '/assets/icons/icon-192x192.png',
   '/assets/icons/icon-512x512.png',
 ];
-self.addEventListener('install', function (event) {
+
+const CACHE_NAME = 'static-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v1';
+
+// install event handler
+self.addEventListener('install', (event) => {
   // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('Opened cache');
-      return cache.addAll(urlsToCache);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
 });
-self.addEventListener('fetch', function (event) {
+
+// // The activate handler takes care of cleaning up old caches.
+// self.addEventListener('activate', (event) => {
+//   const currentCaches = [CACHE_NAME, DATA_CACHE_NAME];
+//   event.waitUntil(
+//     caches
+//       .keys()
+//       .then((cacheNames) => {
+//         return cacheNames.filter(
+//           (cacheName) => !currentCaches.includes(cacheName)
+//         );
+//       })
+//       .then((cachesToDelete) => {
+//         return Promise.all(
+//           cachesToDelete.map((cacheToDelete) => {
+//             return caches.delete(cacheToDelete);
+//           })
+//         );
+//       })
+//       .then(() => self.clients.claim())
+//   );
+// });
+
+// fetch assets from cache
+self.addEventListener('fetch', (event) => {
   // cache all get requests to /api routes
-  if (event.request.url.includes('/api/')) {
+  if (event.request.url.includes('/api')) {
     event.respondWith(
       caches
         .open(DATA_CACHE_NAME)
